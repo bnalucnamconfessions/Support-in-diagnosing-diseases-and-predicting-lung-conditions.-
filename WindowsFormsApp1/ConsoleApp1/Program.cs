cbe4202e099da4308d8389cb715de28080e353b8
BnA Lục Nam Confessions <129104047+bnalucnamconfessions@users.noreply.github.com>
2025-03-05 17:01:15 +0700

    Add files via upload

---
+++ WindowsFormsApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<trieuchung2> a = new List<trieuchung2>();
+            for (int i = 0; i < 4; i++)
+            {
+                trieuchung2 x = new trieuchung2();
+                x.matrieuchung = i + "";
+                x.tentrieuchung = "hiv" + i;
+                a.Add(x);
+            }
+            trieuchung2 y = new trieuchung2();
+            y.matrieuchung = "2";
+            y.tentrieuchung = "hiv2";
+            a.Remove(y);
+            foreach (trieuchung2 x in a)
+            {
+                Console.WriteLine(x.matrieuchung +""+ x.tentrieuchung);
+
+            }
+            //List<string> a = new List<string>();
+            //a.Add("Khong");
+            //a.Add("Mot");
+            //a.Add("Hai");
+            //a.Add("Ba Bon");
+
+            //foreach(string i in a)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WindowsFormsApp1/ConsoleApp1/trieuchung.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    class trieuchung
+    {
+        public string matrieuchung { get; set; }
+        public string tentrieuchung { get; set; }
+    }
+
